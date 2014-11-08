0dcfdcd8b596fd6d494a8cb8ea56d3171788df35
Vitaliy Shishorin <moskrc@gmail.com>
2014-11-09 00:15:36 +0300

    add config.py

--- src/app.py
+++ src/app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    app.logger.debug('test logger')
     return render_template('index.html', name='test')
 
 
 if __name__ == '__main__':
     app.debug = True
+    app.config.from_object('config')
     app.run()
